4f58eb36af206fd481089df7ea5e9c46b0370ca9
megamind-pixel <richardkanyira@gmail.com>
2026-06-08 22:38:12 +0300

    Fix Express 5 catch-all route syntax

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use('/api/payments', require('./routes/paymentRoutes'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../build')));
 
-  app.get('*', (req, res) => {
+  app.get('(.*)', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'));
   });
 } else {
